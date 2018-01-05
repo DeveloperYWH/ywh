1cdf7e91580aa46a68283f36f9cfd6b9b20599f0
sleevewind <303804940@qq.com>
2018-01-05 14:31:48 +0800

    完成购物车页面

---
+++ latte-ec/src/main/java/com/zuimeng/hughfowl/latee/ec/main/cart/ShopCartDelegate.java
@@ -0,0 +1,25 @@
+package com.zuimeng.hughfowl.latee.ec.main.cart;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.zuimeng.hughfowl.latee.ec.R;
+import com.zuimeng.hughfowl.latte.delegates.bottom.BottomItemDelegate;
+
+/**
+ * Created by Rhapsody on 2018/1/5.
+ */
+
+public class ShopCartDelegate extends BottomItemDelegate {
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_shop_cart;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
+
+    }
+}

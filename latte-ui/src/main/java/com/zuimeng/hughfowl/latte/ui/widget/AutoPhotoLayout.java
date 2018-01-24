ac7f04643c936089cf8a654b8a4b5dfa4ed476ef
sleevewind <303804940@qq.com>
2018-01-24 23:59:43 +0800

    评价页面完成

---
+++ latte-ui/src/main/java/com/zuimeng/hughfowl/latte/ui/widget/AutoPhotoLayout.java
@@ -0,0 +1,288 @@
+package com.zuimeng.hughfowl.latte.ui.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.joanzapata.iconify.widget.IconTextView;
+import com.zuimeng.hughfowl.latte.delegates.LatteDelegate;
+import com.zuimeng.hughfowl.latte_ui.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Rhapsody
+ */
+
+public final class AutoPhotoLayout extends LinearLayoutCompat {
+
+    private int mCurrentNum = 0;
+    private final int mMaxNum;
+    private final int mMaxLineNum;
+    private IconTextView mIconAdd = null;
+    private LayoutParams mParams = null;
+    //要删除的图片ID
+    private int mDeleteId = 0;
+    //选中的图片ID
+    private int mSelected = 0;
+    private AppCompatImageView mTargetImageVew = null;
+    private final int mImageMargin;
+    private LatteDelegate mDelegate = null;
+    private ArrayList<View> mLineViews = null;
+    private ArrayList<View> mLine_2 = null;
+    private AlertDialog mTargetDialog = null;
+    private static final String ICON_TEXT = "{fa-plus}";
+    private final float mIconSize;
+
+    private final ArrayList<Integer> LINE_HEIGHTS = new ArrayList<>();
+
+    //防止多次的测量和布局过程
+    private boolean mIsOnceInitOnMeasure = false;
+    private boolean mHasInitOnLayout = false;
+
+    private static final RequestOptions OPTIONS = new RequestOptions()
+            .centerCrop()
+            .diskCacheStrategy(DiskCacheStrategy.NONE);
+
+    public AutoPhotoLayout(Context context) {
+        this(context, null);
+    }
+
+    public AutoPhotoLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public AutoPhotoLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        final TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.camera_flow_layout);
+        mMaxNum = typedArray.getInt(R.styleable.camera_flow_layout_max_count, 6);
+        mMaxLineNum = typedArray.getInt(R.styleable.camera_flow_layout_line_count, 3);
+        mImageMargin = typedArray.getInt(R.styleable.camera_flow_layout_item_margin, 5);
+        mIconSize = typedArray.getDimension(R.styleable.camera_flow_layout_icon_size, 20);
+        typedArray.recycle();
+    }
+
+    public final void setDelegate(LatteDelegate delegate) {
+        this.mDelegate = delegate;
+    }
+
+    public final void onCropTarget(Uri uri) {
+        createNewImageView();
+        Glide.with(mDelegate)
+                .load(uri)
+                .apply(OPTIONS)
+                .into(mTargetImageVew);
+    }
+
+    private void createNewImageView() {
+        mTargetImageVew = new AppCompatImageView(getContext());
+        mTargetImageVew.setId(mCurrentNum);
+        mTargetImageVew.setLayoutParams(mParams);
+        mTargetImageVew.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //获取要删除的图片ID
+                mDeleteId = v.getId();
+                mTargetDialog.show();
+                final Window window = mTargetDialog.getWindow();
+                if (window != null) {
+                    window.setContentView(R.layout.dialog_image_click_panel);
+                    window.setGravity(Gravity.BOTTOM);
+                    window.setWindowAnimations(R.style.anim_panel_up_from_bottom);
+                    window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    final WindowManager.LayoutParams params = window.getAttributes();
+                    params.width = WindowManager.LayoutParams.MATCH_PARENT;
+                    params.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+                    params.dimAmount = 0.5f;
+                    window.setAttributes(params);
+                    window.findViewById(R.id.dialog_image_clicked_btn_delete)
+                            .setOnClickListener(new OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    //得到要删除的图片
+                                    final AppCompatImageView deleteImageViwe =
+                                            findViewById(mDeleteId);
+                                    //设置图片逐渐消失的动画
+                                    final AlphaAnimation animation = new AlphaAnimation(1, 0);
+                                    animation.setDuration(500);
+                                    animation.setRepeatCount(0);
+                                    animation.setFillAfter(true);
+                                    animation.setStartOffset(0);
+                                    deleteImageViwe.setAnimation(animation);
+                                    animation.start();
+                                    AutoPhotoLayout.this.removeView(deleteImageViwe);
+                                    mCurrentNum -= 1;
+                                    //当数目达到上限时隐藏添加按钮，不足时显示
+                                    if (mCurrentNum < mMaxNum) {
+                                        mIconAdd.setVisibility(VISIBLE);
+                                        final AppCompatImageView SelectedView =
+                                                findViewById(mDeleteId);
+                                    }
+                                    mTargetDialog.cancel();
+                                }
+                            });
+                    window.findViewById(R.id.dialog_image_clicked_btn_cancel)
+                            .setOnClickListener(new OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    mTargetDialog.cancel();
+                                }
+                            });
+                }
+            }
+        });
+        //添加子View的时候传入位置
+        this.addView(mTargetImageVew, mCurrentNum);
+        mCurrentNum++;
+        //当添加数目大于mMaxNum时，自动隐藏添加按钮
+        if (mCurrentNum >= mMaxNum) {
+            mIconAdd.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        final int sizeWith = MeasureSpec.getSize(widthMeasureSpec);
+        final int modeWith = MeasureSpec.getMode(widthMeasureSpec);
+        final int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
+        final int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+        //wrap_content
+        int width = 0;
+        int height = 0;
+        //记录每一行的宽度与高度
+        int lineWith = 0;
+        int lineHeight = 0;
+        //得到内部元素个数
+        int cCount = getChildCount();
+        for (int i = 0; i < cCount; i++) {
+            final View child = getChildAt(i);
+            //测量子View的宽和高
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+            //得到LayoutParams
+            final MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            //子View占据的宽度
+            final int childWidth = child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            //子View占据的高度
+            final int childHeight = child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+            //换行
+            if (lineWith + childWidth > sizeWith - getPaddingLeft() - getPaddingRight()) {
+                //对比得到最大宽度
+                width = Math.max(width, lineWith);
+                //重置lineWidth
+                lineWith = childWidth;
+                height += lineHeight;
+                lineHeight = childHeight;
+            } else {
+                //未换行
+                //叠加行宽
+                lineWith += childWidth;
+                //得到当前最大的高度
+                lineHeight = Math.max(lineHeight, childHeight);
+            }
+            //最后一个子控件
+            if (i == cCount - 1) {
+                width = Math.max(lineWith, width);
+                //判断是否超过最大拍照限制
+                height += lineHeight;
+            }
+        }
+        setMeasuredDimension(
+                modeWith == MeasureSpec.EXACTLY ? sizeWith : width + getPaddingLeft() + getPaddingRight(),
+                modeHeight == MeasureSpec.EXACTLY ? sizeHeight : height + getPaddingTop() + getPaddingBottom()
+        );
+        //设置一行所有图片的宽高
+        final int imageSideLen = sizeWith / mMaxLineNum;
+        //只初始化一次
+        if (!mIsOnceInitOnMeasure) {
+            mParams = new LayoutParams(imageSideLen, imageSideLen);
+            mIsOnceInitOnMeasure = true;
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+        LINE_HEIGHTS.clear();
+        int lineHeight = 0;
+        if (!mHasInitOnLayout) {
+            mLineViews = new ArrayList<>();
+            mHasInitOnLayout = true;
+        }
+        final int cCount = getChildCount();
+        for (int i = 0; i < cCount; i++) {
+            final View child = getChildAt(i);
+            final MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            final int childHeight = child.getMeasuredHeight();
+            lineHeight = Math.max(lineHeight, childHeight + lp.topMargin + lp.bottomMargin);
+            mLineViews.add(child);
+        }
+        LINE_HEIGHTS.add(lineHeight);
+        //设置子View位置
+        int left = getPaddingLeft();
+        int top = getPaddingTop();
+        //行数
+            final int size = mLineViews.size();
+            for (int j = 0; j < size; j++) {
+                final View child = mLineViews.get(j);
+                //判断child的状态
+                if (child.getVisibility() == GONE) {
+                    continue;
+                }
+                if(j>=mMaxLineNum)
+                {
+                    left = child.getMeasuredWidth()*(j-mMaxLineNum);
+                    top = lineHeight*(j/mMaxLineNum);
+                }
+                final MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+                //设置子View的边距
+                final int lc = left + lp.leftMargin;
+                Log.d("lcaaa",String.valueOf(lc));
+                final int tc = top + lp.topMargin;
+                Log.d("tc",String.valueOf(tc));
+                final int rc = lc + child.getMeasuredWidth() - mImageMargin;
+                final int bc = tc + child.getMeasuredHeight();
+                //为子View进行布局
+                child.layout(lc, tc, rc, bc);
+                left += child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            }
+        mIconAdd.setLayoutParams(mParams);
+        mHasInitOnLayout = false;
+    }
+
+    private void initAddIcon() {
+        mIconAdd = new IconTextView(getContext());
+        mIconAdd.setText(ICON_TEXT);
+        mIconAdd.setGravity(Gravity.CENTER);
+        mIconAdd.setTextSize(mIconSize);
+        mIconAdd.setBackgroundResource(R.drawable.border_text);
+        mIconAdd.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDelegate.startCameraWithCheck();
+            }
+        });
+        this.addView(mIconAdd);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        initAddIcon();
+        mTargetDialog = new AlertDialog.Builder(getContext()).create();
+    }
+}

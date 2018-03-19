3c2f2e26e444b05fdff3060af000a6e4739fd78c
sleevewind <303804940@qq.com>
2018-03-19 18:08:01 +0800

    店铺

--- latte-ec/src/main/java/com/zuimeng/hughfowl/latee/ec/main/personal/PersonalDelegate.java
+++ latte-ec/src/main/java/com/zuimeng/hughfowl/latee/ec/main/personal/PersonalDelegate.java
@@ -1,16 +1,23 @@
 package com.zuimeng.hughfowl.latee.ec.main.personal;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
+import com.avos.avoscloud.AVUser;
 import com.avos.avoscloud.FindCallback;
 import com.bumptech.glide.Glide;
 import com.zuimeng.hughfowl.latee.ec.R;
@@ -24,7 +31,8 @@ import com.zuimeng.hughfowl.latee.ec.main.personal.list.ListItemType;
 import com.zuimeng.hughfowl.latee.ec.main.personal.order.OrderListDelegate;
 import com.zuimeng.hughfowl.latee.ec.main.personal.profile.UserProfileDelegate;
 import com.zuimeng.hughfowl.latee.ec.main.personal.settings.SettingsDelegate;
-import com.zuimeng.hughfowl.latee.ec.shop.CheckDelegate;
+import com.zuimeng.hughfowl.latee.ec.shop.ShopBottomDelegate;
+import com.zuimeng.hughfowl.latee.ec.shop.myshop.create_shop.CreateShopDelegate;
 import com.zuimeng.hughfowl.latte.delegates.bottom.BottomItemDelegate;
 import com.zuimeng.hughfowl.latte.ui.loader.LatteLoader;
 
@@ -52,6 +60,8 @@ public class PersonalDelegate extends BottomItemDelegate {
 
     public static final String ORDER_TYPE = "ORDER_TYPE";
     private Bundle mArgs = null;
+    Number user_right = 0;
+    private AlertDialog mDialog = null;
 
     @Override
     public Object setLayout() {
@@ -85,6 +95,9 @@ public class PersonalDelegate extends BottomItemDelegate {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, @NonNull final View rootView) {
+
+        this.mDialog = new AlertDialog.Builder(PersonalDelegate.this.getContext()).create();
+
         //获取头像
         final AVQuery<AVObject> query = new AVQuery<>("User_avater");
         LatteLoader.showLoading(getContext());
@@ -130,47 +143,104 @@ public class PersonalDelegate extends BottomItemDelegate {
             }
         });
 
+        final AVQuery<AVUser> avQuery = new AVQuery<>("_User");
+        LatteLoader.showLoading(getContext());
+        avQuery.whereEqualTo("mobilePhoneNumber",
+                String.valueOf(DatabaseManager
+                        .getInstance()
+                        .getDao()
+                        .queryBuilder()
+                        .listLazy()
+                        .get(0)
+                        .getUserId()));
+        avQuery.findInBackground(new FindCallback<AVUser>() {
+            @Override
+            public void done(List<AVUser> list, AVException e) {
 
-        final ListBean address = new ListBean.Builder()
-                .setItemType(ListItemType.ITEM_NORMAL)
-                .setId(1)
-                .setDelegate(new AddressDelegate())
-                .setText("收货地址")
-                .build();
+                user_right = list.get(0).getNumber("user_type");
 
-        final ListBean bill_date = new ListBean.Builder()
-                .setItemType(ListItemType.ITEM_NORMAL)
-                .setId(3)
-                .setDelegate(new BillDateDelegate())
-                .setText("尾款日历")
-                .build();
+                final List<ListBean> data = new ArrayList<>();
+                final ListBean address = new ListBean.Builder()
+                        .setItemType(ListItemType.ITEM_NORMAL)
+                        .setId(1)
+                        .setDelegate(new AddressDelegate())
+                        .setText("收货地址")
+                        .build();
 
-        final ListBean system = new ListBean.Builder()
-                .setItemType(ListItemType.ITEM_NORMAL)
-                .setId(2)
-                .setDelegate(new SettingsDelegate())
-                .setText("系统设置")
-                .build();
+                final ListBean bill_date = new ListBean.Builder()
+                        .setItemType(ListItemType.ITEM_NORMAL)
+                        .setId(3)
+                        .setDelegate(new BillDateDelegate())
+                        .setText("尾款日历")
+                        .build();
 
-        final ListBean my_shop = new ListBean.Builder()
-                .setItemType(ListItemType.ITEM_NORMAL)
-                .setId(4)
-                .setDelegate(new CheckDelegate())
-                .setText("我的店铺")
-                .build();
+                final ListBean system = new ListBean.Builder()
+                        .setItemType(ListItemType.ITEM_NORMAL)
+                        .setId(2)
+                        .setDelegate(new SettingsDelegate())
+                        .setText("系统设置")
+                        .build();
 
+                data.add(address);
+                data.add(bill_date);
+                data.add(system);
+                if ((int) user_right != 1) {
+                    final ListBean my_shop = new ListBean.Builder()
+                            .setItemType(ListItemType.ITEM_NORMAL)
+                            .setId(4)
+                            .setDelegate(new ShopBottomDelegate())
+                            .setText("我的店铺")
+                            .build();
+                    data.add(my_shop);
+                }
 
-        final List<ListBean> data = new ArrayList<>();
-        data.add(address);
-        data.add(bill_date);
-        data.add(system);
-        data.add(my_shop);
+                if ((int) user_right == 1) {
+                    final ListBean my_shop = new ListBean.Builder()
+                            .setItemType(ListItemType.ITEM_NORMAL)
+                            .setId(4)
+                            .setText("我的店铺")
+//                            .setCheckDelegate(new CheckDelegate(PersonalDelegate.this))
+                            .build();
+                    data.add(my_shop);
+                }
 
-        //设置RecyclerView
-        final LinearLayoutManager manager = new LinearLayoutManager(getContext());
-        mRvSettings.setLayoutManager(manager);
-        final ListAdapter adapter = new ListAdapter(data);
-        mRvSettings.setAdapter(adapter);
-        mRvSettings.addOnItemTouchListener(new PersonalClickListener(this));
+                //设置RecyclerView
+                final LinearLayoutManager manager = new LinearLayoutManager(getContext());
+                mRvSettings.setLayoutManager(manager);
+                final ListAdapter adapter = new ListAdapter(data);
+                mRvSettings.setAdapter(adapter);
+                mRvSettings.addOnItemTouchListener(new PersonalClickListener(PersonalDelegate.this));
+            }
+        });
+    }
+
+    public void beginCreatDialog() {
+        mDialog.show();
+        final Window window = mDialog.getWindow();
+        if (window != null) {
+            window.setContentView(R.layout.dialog_creat_panel);
+            window.setGravity(Gravity.BOTTOM);
+            window.setWindowAnimations(R.style.anim_panel_up_from_bottom);
+            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            //设置属性
+            final WindowManager.LayoutParams params = window.getAttributes();
+            params.width = WindowManager.LayoutParams.MATCH_PARENT;
+            params.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+            window.setAttributes(params);
+
+            window.findViewById(R.id.dialog_creat_shop).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    getParentDelegate().getSupportDelegate().start(new CreateShopDelegate());
+                    mDialog.cancel();
+                }
+            });
+            window.findViewById(R.id.dialog_creat_cancel).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mDialog.cancel();
+                }
+            });
+        }
     }
 }

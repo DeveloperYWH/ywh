fb548221c57052342f1c8fe467971f1861bfa041
DeveloperYWH <765852613@qq.com>
2018-03-20 16:42:27 +0800

    moments

--- latte-ec/src/main/java/com/zuimeng/hughfowl/latee/ec/main/explorer/moments/MomentsDelegate.java
+++ latte-ec/src/main/java/com/zuimeng/hughfowl/latee/ec/main/explorer/moments/MomentsDelegate.java
@@ -1,19 +1,15 @@
 package com.zuimeng.hughfowl.latee.ec.main.explorer.moments;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
 
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
 import com.avos.avoscloud.FindCallback;
-import com.sackcentury.shinebuttonlib.ShineButton;
 import com.zuimeng.hughfowl.latee.ec.R;
 import com.zuimeng.hughfowl.latee.ec.R2;
 import com.zuimeng.hughfowl.latte.delegates.LatteDelegate;
